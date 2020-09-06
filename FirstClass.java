9afb568c3e1398a7aff8052709fa0240a6ee3e75
sergik3001 <sergik3001@gmail.com>
2020-09-06 21:01:33 +0300

    Create FirstClass.java
    Task 1

---
+++ FirstClass.java
@@ -0,0 +1,22 @@
+package siarhei.klybik.firstapp;
+
+public class FirstClass {
+    public	static	void	main(String[]	args) {
+        int i;
+        double s, z;
+        i = 3;
+        s = 5.5;
+        z = s / i;
+        System.out.println("i	=	" + i);
+        System.out.println("s	=	" + s);
+        System.out.printf("z	=	%f", z);
+        System.out.println();
+        for (int f = 0; f < 20; f++) {
+            if (f%2 == 0 && f/2 >=5) {
+                System.out.println(f);
+            }
+        }
+
+    }
+}
+
